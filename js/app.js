2f7bca97740f8865b45cbb7b44c46ae457f4073f
Murilo Miranda <biomurilodantas@gmail.com>
2018-03-07 11:11:07 +0100

    Cards selector works

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // Start the game click the button "Start!"
-var btn = document.querySelector('#start');
-btn.addEventListener('click', startGame);
+// const btn = document.querySelector('#start');
+document.addEventListener('DOMContentLoaded', startGame);
 
 // Display the cards on the page
 function startGame() {
@@ -51,11 +51,73 @@ function shuffle(array) {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
+let openCards = [];
+let counter = 0;
+let numberMatches = 0
+let deck = document.querySelector('.deck');
+deck.addEventListener('click', cardSelection);
 
- // Reset the Game
- var btn_restart = document.querySelector('.restart');
- btn_restart.addEventListener('click', restartGame);
+function isMatch(){
+  // check if there are two cards
+  if(openCards.length === 2){
+    // if the cards match
+    if(openCards[0] === openCards[1]){
+      //lock the cards in the open position
+      const matchCards = document.getElementsByClassName(openCards[0]);
+      Array.from(matchCards).forEach(function(card){
+        card.parentElement.classList.toggle("show");
+        card.parentElement.classList.toggle("open");
+        card.parentElement.classList.add("match");
+      });
+      numberMatches = numberMatches + 1;
+    }else{ // cards do not match
+
+      const noMatchCard1 = document.getElementsByClassName(openCards[0]);
+      Array.from(noMatchCard1).forEach(function(card){
+        card.parentElement.classList.remove("show");
+        card.parentElement.classList.remove("open");
+      });
+      const noMatchCard2 = document.getElementsByClassName(openCards[1]);
+      Array.from(noMatchCard2).forEach(function(card){
+        card.parentElement.classList.remove("show");
+        card.parentElement.classList.remove("open");
+      });
+    }
+
+    // increment the move counter and display it on the page
+    counter = counter + 1;
+    const numberMoves = document.querySelector(".moves");
+    numberMoves.textContent = counter;
+    openCards = [];
+  }
+}
+
+function cardSelection(evt){
+  let target = evt.target;
+  // Add classes "show" and "open" to a selected card
+  if(target.nodeName === "LI"){
+    target.classList.toggle("show");
+    target.classList.toggle("open");
+    openCards.push(target.firstElementChild.classList[1]);
+  }else if(target.nodeName === "I"){
+    target.parentElement.classList.toggle("show");
+    target.parentElement.classList.toggle("open");
+    openCards.push(target.classList[1]);
+  }
+
+  isMatch();
+}
+
+// Reset the Game
+const btn_restart = document.querySelector('.restart');
+btn_restart.addEventListener('click', restartGame);
 
 function restartGame(){
+  const matchClass = document.querySelectorAll(".match");
+  Array.from(matchClass).forEach(function(card){
+    card.classList.remove("match");
+  });
+  const numberMoves = document.querySelector(".moves");
+  numberMoves.textContent = 0;
   startGame();
 }
